26797d4a9eb6b9ef55dd801aa70352d089f0ad62
Boris Däppen <boris_daeppen@bluewin.ch>
2010-01-19 13:05:24 +0100

    Added a lot of fancy features to the GUI. Also added licence information.

--- orgcreatorgtk.pl
+++ orgcreatorgtk.pl
@@ -1,7 +1,25 @@
 #!/usr/bin/perl
 
+# Copyright 2010 Boris Daeppen <boris_daeppen@bluewin.ch>
+# 
+# This file is part of orgcreator.
+# 
+# orgcreator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# orgcreator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with orgcreator.  If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
+use Gtk2::Gdk::Keysyms;
 use Glib qw(TRUE FALSE);
 
 use Gtk2 -init;
@@ -10,6 +28,8 @@ use Gtk2 -init;
 ##########################
 my $window = Gtk2::Window->new ('toplevel');
 $window->signal_connect (delete_event => sub { Gtk2->main_quit });
+$window->signal_connect ('key-press-event' => \&on_key_pressed );
+
 my $main_box = Gtk2::VBox->new();
 my $split_box = Gtk2::HBox->new();
 my $input_frame = Gtk2::Frame->new('Input');
@@ -22,15 +42,22 @@ my $config_box = Gtk2::VBox->new(2,10);
 ###############
 my $menubar = Gtk2::MenuBar->new();
 my $menu_file = Gtk2::Menu->new();
+my $menu_help = Gtk2::Menu->new();
 my $menuitem_file = Gtk2::MenuItem->new('File');
 my $menuitem_help = Gtk2::MenuItem->new('Help');
 
 my $menuitem_quit = Gtk2::MenuItem->new('Quit');
+my $menuitem_about = Gtk2::MenuItem->new('About');
 $menuitem_quit->signal_connect(activate => sub { Gtk2->main_quit });
+#$menuitem_about->signal_connect(activate => sub { Gtk2->main_quit });
+#$menuitem_about->signal_connect(activate => sub { \&on_about_clicked() });
+$menuitem_about->signal_connect(activate => \&on_about_clicked );
 
 $menu_file->add($menuitem_quit);
+$menu_help->add($menuitem_about);
 
 $menuitem_file->set_submenu($menu_file);
+$menuitem_help->set_submenu($menu_help);
 
 $menubar->add($menuitem_file);
 $menubar->add($menuitem_help);
@@ -95,7 +122,7 @@ foreach my $name (@radio_label) {
 ################
 my $button_run = Gtk2::Button->new('run program');
 $config_box->add($button_run);
-$button_run->signal_connect(clicked => \&on_run_clicked);
+$button_run->signal_connect(clicked => \&on_run_clicked );
 
 # Put GUI together #
 ####################
@@ -122,10 +149,12 @@ Gtk2->main;
 ###############
 # SUBROUTINES #
 ###############
-sub on_run_clicked {
-    my $button = shift;
-    my $argumentlist = '';
 
+# fetch the user input and pass it to the program
+sub on_run_clicked {
+
+    # concatenate all arguments together
+    my $argumentlist = '';
     while (my ($option, $entry) = each %inputdata) {
         next if ($option =~ /path/);
         my $value = $entry->get_text();
@@ -142,11 +171,70 @@ sub on_run_clicked {
             }
         }
     }
-    my $command = "orgcreator $argumentlist";
-    print "$command        --\n";
 
+    # launch program with arguments fetched from GUI
+    my $command = "orgcreator $argumentlist";
     my $output = `bash -c '$command' 2>&1`;
 
-    $output_textview->get_buffer()->set_text("The following command was executed:\n\n$command\n\n\nThe output was as following:\n\n$output\n");
+    # print the output to the GUI.
+    $output_textview->get_buffer()->set_text($output);
+    # Create a MessageDialog with Pango-Markup
+#    my $dialog = Gtk2::MessageDialog->new_with_markup ($window, 'destroy-with-parent', 'info', 'ok', "<b>The output was as following:</b>\n$output");
+#    my $response = $dialog->run;
+#    print "$response\n";
+#    $dialog->destroy;
 
+}
+
+# Display a small window with information
+sub on_about_clicked {
+    print "About clicked\n";
+    
+    my $text = "<b>ORGCREATOR</b>
+<i>create a company organigram out of your OrangeHRM data.</i>
+
+Copyright (C) 2010 Boris Daeppen &lt;boris_daeppen\@bluewin.ch&gt;
+
+<small>This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;</small>
+
+For further information see also:
+http://github.com/borisdaeppen/orgcreator
+http://sourceforge.net/projects/orangehrm";
+
+    # Create a MessageDialog with Pango-Markup
+    my $dialog = Gtk2::MessageDialog->new_with_markup ($window, 'destroy-with-parent', 'info', 'ok', $text);
+    my $response = $dialog->run;
+    print "$response\n";
+    $dialog->destroy;
+
+}
+
+# handle the key RETURN like the run button
+sub on_key_pressed {
+    my ($widget, $event,$parameter)= @_;
+
+    my $key_nr = $event->keyval();
+    foreach my $key  (keys %Gtk2::Gdk::Keysyms){
+        
+        my $key_compare = $Gtk2::Gdk::Keysyms{$key};
+                
+        if($key_compare == $key_nr){
+            if ($key =~ /Return/) {
+                on_run_clicked();
+            }
+            last;
+        }
+    }
+    return FALSE;
 }
--- orgcreatorx.sh
+++ orgcreatorx.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright 2010 Boris Daeppen <boris_daeppen@bluewin.ch>
+# 
+# This file is part of orgcreator.
+# 
+# orgcreator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# orgcreator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with orgcreator.  If not, see <http://www.gnu.org/licenses/>.
+
 # to avoid a known bug I need to export this variable
 # see https://bugs.launchpad.net/ubuntu/+source/libcanberra/+bug/368175
 export XDIALOG_NO_GMSGS=1
